903bf9640a3c6d6392070a691dacd37233265881
Stephane Landelle <slandelle@excilys.com>
2011-03-22 14:35:34 +0100

    adding tests for LogbackConfigurer

---
+++ src/test/java/com/excilys/utils/spring/log/logback/LogbackConfigurerTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2010-2011 Excilys (www.excilys.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.excilys.utils.spring.log.logback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test for {@link LogbackConfigurer}
+ * 
+ * 
+ * @author <a href="mailto:slandelle@excilys.com">Stephane LANDELLE</a>
+ */
+public class LogbackConfigurerTest {
+
+	private Logger logger = LoggerFactory.getLogger(LogbackConfigurerTest.class);
+
+	@BeforeClass
+	public static void initLogback() throws Exception {
+		LogbackConfigurer.initLogging("classpath:com/excilys/utils/spring/log/logback/logback-test.xml");
+	}
+
+	@AfterClass
+	public static void shutDownLogback() {
+		LogbackConfigurer.shutdownLogging();
+	}
+
+	/**
+	 * The logback-test.xml defines a Console output. Console output is
+	 * temporarily trapped so that we can check what is written.
+	 */
+	@Test
+	public void testConsoleLogger() {
+
+		PrintStream stdout = System.out;
+		String log = null;
+
+		try {
+			// replace console standard output
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			PrintStream ps = new PrintStream(os);
+			System.setOut(ps);
+
+			logger.debug("something in DEBUG");
+
+			log = os.toString();
+
+		} finally {
+			// restore console standard output
+			System.setOut(stdout);
+		}
+
+		Assert.assertTrue("Output log is incorrect", log.contains("DEBUG c.e.u.s.l.l.LogbackConfigurerTest.testConsoleLogger - something in DEBUG"));
+	}
+}
---
+++ src/test/java/com/excilys/utils/spring/log/logback/LogbackConfigurerTestExecutionListenerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2010-2011 Excilys (www.excilys.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.excilys.utils.spring.log.logback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.excilys.utils.spring.log.logback.test.Logback;
+import com.excilys.utils.spring.log.logback.test.LogbackConfigurerTestExecutionListener;
+import com.excilys.utils.spring.log.slf4j.InjectLogger;
+
+/**
+ * Test for {@link LogbackConfigurer}
+ * 
+ * 
+ * @author <a href="mailto:slandelle@excilys.com">Stephane LANDELLE</a>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({ LogbackConfigurerTestExecutionListener.class, DependencyInjectionTestExecutionListener.class })
+@ContextConfiguration("applicationContext.xml")
+@Logback
+public class LogbackConfigurerTestExecutionListenerTest {
+
+	@InjectLogger
+	private Logger logger;
+
+	/**
+	 * The logback-test.xml defines a Console output. Console output is
+	 * temporarily trapped so that we can check what is written.
+	 */
+	@Test
+	public void testConsoleLogger() {
+
+		PrintStream stdout = System.out;
+		String log = null;
+
+		try {
+			// replace console standard output
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			PrintStream ps = new PrintStream(os);
+			System.setOut(ps);
+
+			logger.debug("something in DEBUG");
+
+			log = os.toString();
+
+		} finally {
+			// restore console standard output
+			System.setOut(stdout);
+		}
+
+		Assert.assertTrue("Output log is incorrect", log.contains("DEBUG c.e.u.s.l.l.LogbackConfigurerTestExecutionListenerTest.testConsoleLogger - something in DEBUG"));
+	}
+}
